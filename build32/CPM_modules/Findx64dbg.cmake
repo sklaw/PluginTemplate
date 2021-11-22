75375fa9568e390ccc27fe24836e7e5094eb75b9
S.K. Law <sk.law.lsq@gmail.com>
2021-11-22 01:00:28 -0500

    track build folders

---
+++ build32/CPM_modules/Findx64dbg.cmake
@@ -0,0 +1,3 @@
+include(C:/Users/test/Documents/GitHub/PluginTemplate/cmake/CPM.cmake)
+CPMAddPackage(NAME;x64dbg;URL;https://downloads.sourceforge.net/project/x64dbg/snapshots/snapshot_2021-05-08_14-17.zip;URL_HASH;SHA1=a46f3bf3f84fee3b1f7da8949e79d425d7294979;DOWNLOAD_ONLY;ON)
+set(x64dbg_FOUND TRUE)
